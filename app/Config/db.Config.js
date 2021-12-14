23691973d7a86fef8013eaf0099cad8ab758a49a
Lê Minh Chiến <chienlm@vnpt.vn>
2021-12-14 10:16:23 +0700

    update db mongo cluster

--- app/Config/db.Config.js
+++ app/Config/db.Config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/QLNV_db"
+    //url: "mongodb://localhost:27017/QLNV_db"
+    url: "mongodb+srv://chienlm:123456aA%40@cluster0.juvve.mongodb.net/test"
   };
